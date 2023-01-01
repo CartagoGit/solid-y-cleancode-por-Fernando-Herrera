2a461a80d371095e6d42c506e0f092c82f344bf2
Cartago <cartago.relaxingcup@gmail.com>
2023-01-01 23:20:14 +0100

    02-11- Nombres de variables

--- projects/01-inicio/src/clean-code/01-names.ts
+++ projects/01-inicio/src/clean-code/01-names.ts
@@ -1,55 +1,48 @@
 (() => {
+	// Ejemplo
+	// Archivos a evaluar - files to evaluate
+	const filesToEvaluate = [
+		{ id: 1, toDelete: false },
+		{ id: 2, toDelete: false },
+		{ id: 3, toDelete: true },
+		{ id: 4, toDelete: false },
+		{ id: 5, toDelete: false },
+		{ id: 7, toDelete: true },
+	] as { id: number; toDelete: boolean }[];
 
-    // Ejemplo
-    // Archivos a evaluar - files to evaluate
-    const filesToEvaluate = [
-        { id: 1, flagged: false },
-        { id: 2, flagged: false },
-        { id: 3, flagged: true },
-        { id: 4, flagged: false },
-        { id: 5, flagged: false },
-        { id: 7, flagged: true },
-    ]; 
-    
-    // Archivos marcados para borrar - files to delete
-    const filesToDelete = filesToEvaluate.map( file => file.flagged );
+	// Archivos marcados para borrar - files to delete
+	const filesToDelete: boolean[] = filesToEvaluate.map((file) => file.toDelete);
 
-    // Malos
-    class AbstractUser { };
-    class UserMixin { };
-    class UserImplementation { };
-    interface IUser { };
+	// MAL
+	class AbstractUser {}
+	class UserMixin {}
+	class UserImplementation {}
+	interface IUser {}
 
-    // Mejor
-    class User { };
-    interface User { };
+	// Mejor
+	class User {}
+	interface User {}
 
+	// Todo: Tarea
 
-    // Todo: Tarea
-        
-    // día de hoy - today
-    const today = new Date();
-    
-    // días transcurridos - elapsed time in days
-    const elapsedTimeInDays: number = 23;
-    
-    // número de archivos en un directorio - number of files in directory
-    const numberOfFilesInDirectory = 33;
-    
-    // primer nombre - first name
-    const firstName = 'Fernando';
-    
-    // primer apellido - last name
-    const lastName = 'Herrera';
+	// día de hoy - today
+	const today: Date = new Date();
 
-    // días desde la última modificación - days since modification
-    const daysSinceLastModification = 12;
-    
-    // cantidad máxima de clases por estudiante - max classes per student
-    const maxClassesPerStudent = 6;
+	// días transcurridos - elapsed time in days
+	const elapsedDays: number = 23;
 
+	// número de archivos en un directorio - number of files in directory
+	const numFilesInFolder: number = 33;
 
+	// primer nombre - first name
+	const firstName: string = "Fernando";
+
+	// primer apellido - last name
+	const surname: string = "Herrera";
+
+	// días desde la última modificación - days since modification
+	const daysSinceModification = 12;
+
+	// cantidad máxima de clases por estudiante - max classes per student
+	const maxClassesPerStudent: number = 6;
 })();
-
-
-
