784d17a6114eb81166516708fd97142e32f9c956
Cartago <cartago.relaxingcup@gmail.com>
2023-01-02 11:52:33 +0100

    Terminamo el ejercicio 3

--- projects/01-inicio/src/clean-code/03-functions.ts
+++ projects/01-inicio/src/clean-code/03-functions.ts
@@ -1,43 +1,42 @@
 (() => {
+	// función para obtener información de una película por Id
+	function getInfoMovie(movieId: string): object {
+		console.log({ movieId });
+		const info: object = {};
+		return info;
+	}
 
-    function getMovieById( id: string ) {
-        console.log({ id });
-    }
+	// función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+	function getInfoAllActors(movieId: string): [] {
+		console.log({ movieId });
+		return [];
+	}
 
-    function getMovieCastById( id: string ) {
-        console.log({ id });
-    }
+	// funcion para obtener el bio del actor por el id
+	function getBioActor(actorId: string): void {
+		console.log({ actorId });
+	}
 
-    function getActorBioById( id: string ) {
-        console.log({ id });
-    }
-    
-    interface Movie {
-        cast:        string[];
-        description: string;
-        rating:      number;
-        title:       string;
-    }
+	// Crear una película
+	interface Movie {
+		cast: string[];
+		description: string;
+		rating: number;
+		title: string;
+	}
 
-    function createMovie({ title, description, rating, cast }: Movie) {
-        console.log({ title, description, rating, cast });
-    }
+	function creatMovie(movie: Movie): void {
+		console.log(movie);
+	}
 
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        
+	// Crea un nuevo actor
+	function createActor(firstName: string, birthdate: Date): boolean {
+		// tarea asincrona para verificar nombre
+		// ..
+		// ..
+		if (firstName.toLowerCase() === "fernando" || !birthdate) return false;
 
-        if ( fullName === 'fernando' ) return false;
-
-        console.log('Crear actor', birthdate);
-        return true;        
-
-    }
-
-    
-
-
+		console.log("Crear actor");
+		return true;
+	}
 })();
-
-
-
-
